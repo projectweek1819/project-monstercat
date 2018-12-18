b64ba9f0db912c5ea962c909120070fc2749b44d
GermeauSimonÃ <simon.germeau@student.ucll.be>
2018-12-18 13:22:07 +0100

    fixed generate board function, no 3 chains are in the starting grid anymoreÃ

--- Game/sketch.js
+++ Game/sketch.js
@@ -13,6 +13,10 @@ function draw(){
 
 }
 
+function mousePressed(){
+  //// TODO:
+}
+
 function showGrid(){
   for(var i = 0; i < grid.length; i++){
     for(var j = 0; j < grid[i].length; j++){
@@ -28,13 +32,25 @@ function generateBoard(xSize, ySize){
     grid[i] = new Array(xSize);
     for(var j = 0; j < grid[i].length; j++){
       grid[i][j] = new Jewel(i, j, Math.floor(Math.random() * 6) + 1);
+
+      //check horizontal
+      while((i >=2) && (grid[i-1][j].color == grid[i][j].color) &&
+              (grid[i-2][j].color == grid[i][j].color)){
+
+        grid[i][j] = new Jewel(i, j, Math.floor(Math.random() * 6) + 1)
+      }
+      //check vertical
+      while((j >=2) && (grid[i][j-1].color == grid[i][j].color) &&
+              (grid[i][j-2].color == grid[i][j].color)){
+        grid[i][j] = new Jewel(i, j, Math.floor(Math.random() * 6) + 1)
+      }
     }
   }
 }
 
 function swap(Jewel1, Jewel2){
-  backupJewel1 = Jewel;
-  backupJewel2 = Jewe2;
+  backupJewel1 = Jewel1;
+  backupJewel2 = Jewel2;
 
   xCoord1 = Jewel1.x;
   yCoord1 = Jewel1.y;

ef8a6f4b080647a83d1f3466e99dcfcfa5ea3cf8
SateurMaxime <maxime.sateur@student.ucll.be>
2018-12-19 13:40:27 +0100

    Commit

--- Game/sketch.js
+++ Game/sketch.js
@@ -98,9 +98,3 @@ if(isValidSwap(Jewel1, Jewel2)){
     console.log("invalid swap, try again bitch !");
   }
 }
-
-
-function isValidSwap(Jewel1, Jewel2){
-  return Math.abs(Jewel2.x - Jewel1.x) == 1 && Math.abs(Jewel2.y - Jewel1.y) == 0
-  || Math.abs(Jewel2.x - Jewel1.x) == 0 && Math.abs(Jewel2.y - Jewel1.y) == 1
-}
